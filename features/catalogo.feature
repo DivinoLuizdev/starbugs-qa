823c423fd0796c1948b46e4fca741ae2bd483887
DivinoLuizdev <juniorwar360@gmail.com>
2025-07-09 12:44:29 -0300

    Add scenarios for purchasing coffee and handling unavailable products

--- features/catalogo.feature
+++ features/catalogo.feature
@@ -9,3 +9,23 @@ Cenário: Acessar o Catálago de cafés na página principal
 
     Quando acessar a pagina principal da Starbugs
     Então eu devo ver uma lista de cafés disponíveis
+
+@temp
+Cenário: inciar a compra de um café
+
+    Dado que eu estou na página principal da Starbugs
+        E que desejo compra o café "Expresso Gelado"
+        E que esse produto custa R$ 9.99
+        E que o custo de entrega e de R$ 10.00
+    Quando inicio a compra de item
+    Então deve ver a pagina de Checkout com os detalhes do produto 
+        E o valor total da compra deve ser R$ 19.99
+
+@temp
+Cenário: Café indisponivel
+    Dado que eu estou na página principal da Starbugs
+        E que desejo comprar o café "Expresso Cremoso"
+    Quando inicio a compra desse item
+    Então deve ver um pop-up informando que o produto está indisponível
+        E o pop-up deve ter o texto "Produto indisponível"
+        
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -6,5 +6,51 @@ end
 Então('eu devo ver uma lista de cafés disponíveis') do
     products = all('.coffee-item')
     expect(products.size).to be > 0
+end
 
+# ---------------------
+
+
+Dado('que eu estou na página principal da Starbugs') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('que desejo compra o café {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('que esse produto custa R$ {float}') do |float|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('que o custo de entrega e de R$ {float}') do |float|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando('inicio a compra de item') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('deve ver a pagina de Checkout com os detalhes do produto') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('o valor total da compra deve ser R$ {float}') do |float|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('que desejo comprar o café {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando('inicio a compra desse item') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('deve ver um pop-up informando que o produto está indisponível') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('o pop-up deve ter o texto {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
 end
